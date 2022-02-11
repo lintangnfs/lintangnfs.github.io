8d27310af041f9b22d84b1040cf30f3fbbea5bca
Phyllalintang Nafasa <phyllalintang@bigio.id>
2022-02-11 14:57:15 +0700

    add welcome section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import title from './assets/elements/title.png'
+import start from './assets/elements/start-color.png'
 
 function App() {
   return (
     <div className="layout">
-      <div class="grid-container">
-        <div className="main-layout">
-
-        </div>
+      <div className="section-title">
+        <img className="image-title" src={title} alt="title" />
+        <img className="image-button" src={start} alt="start" />
       </div>
     </div>
   );
