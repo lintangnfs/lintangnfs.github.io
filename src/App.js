dfd1f51f2c8884b0c903292246d6452266acebef
Phyllalintang Nafasa <phyllalintang@bigio.id>
2022-02-13 12:23:24 +0700

    add another design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import title from './assets/elements/title.png'
 import start from './assets/elements/start-color.png'
-import daisy from './assets/elements/intro-daisy.png'
+import introduction from './assets/elements/introduction.png'
 
 import './App.css';
 
@@ -10,21 +10,24 @@ function App() {
   const introRef = useRef();
 
   return (
-    <div className="layout">
+    <>
       <div className="section-title">
         <img className="image-title" src={title} alt="title" />
         <div className="stage" onClick={() => introRef.current.scrollIntoView({ behavior: 'smooth' })}>
           <img className="image-button bounce-big" src={start} alt="start" />
         </div>
       </div>
-      <div className="section-intro" ref={introRef}>
-        <img className="img-intro daisy" src={daisy} alt="start" />
-        <p className="img-intro hello">Hello</p>
-        <div className="circle">
-          <p>I’m an art enthusiast who loves to solve problems using code and try to create useful plus beautiful web application.</p>
+      <div className="section-intro">
+        <div className="container-intro">
+          <img className="img-intro absolute-top" src={introduction} alt="start" />
+          <div className="card">
+            <div className="card-content">
+              <p>I’m an art enthusiast who loves to solve problems using code and try to create useful plus beautiful web application.</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
